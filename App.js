5a8b3860320c7dd6bca86f4b064fb59607d4a831
jenny5546 <jenny5546@naver.com>
2020-04-22 17:58:51 +0900

    added calendar

--- App.js
+++ App.js
@@ -1,10 +1,40 @@
 import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, View, Dimensions } from 'react-native';
+import { Calendar } from 'react-native-calendars';
+const { height, width } = Dimensions.get("window");
 
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>E-mory</Text>
+      <Calendar
+        theme={{
+          backgroundColor: '#ffffff',
+          calendarBackground: '#ffffff',
+          textSectionTitleColor: '#b6c1cd',
+          selectedDayBackgroundColor: '#00adf5',
+          selectedDayTextColor: '#ffffff',
+          todayTextColor: '#00adf5',
+          dayTextColor: '#2d4150',
+          textDisabledColor: '#d9e1e8',
+          dotColor: '#00adf5',
+          selectedDotColor: '#ffffff',
+          arrowColor: 'orange',
+          disabledArrowColor: '#d9e1e8',
+          monthTextColor: 'black',
+          indicatorColor: 'blue',
+          textDayFontWeight: '300',
+          textMonthFontWeight: 'bold',
+          textDayHeaderFontWeight: '300',
+          textDayFontSize: 15,
+          textMonthFontSize: 20,
+          textDayHeaderFontSize: 10
+        }}
+        style={{
+          height: height-150,
+          borderWidth: 1,
+          borderColor: 'gray'
+        }}
+      />
     </View>
   );
 }
@@ -12,8 +42,29 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: 'pink',
-    alignItems: 'center',
+    // width: width,
+    // height: height,
+    backgroundColor: '#ffff',
+    // alignItems: 'center',
     justifyContent: 'center',
   },
 });
+
+// import { StyleSheet, Text, View } from 'react-native';
+
+// export default function App() {
+//   return (
+//     <View style={styles.container}>
+//       <Text>E-mory</Text>
+//     </View>
+//   );
+// }
+
+// const styles = StyleSheet.create({
+//   container: {
+//     flex: 1,
+//     backgroundColor: 'pink',
+//     alignItems: 'center',
+//     justifyContent: 'center',
+//   },
+// });
--- components/Calendar.js
+++ components/Calendar.js
@@ -1,1 +1,51 @@
-//Main Page Calendar
+import React from 'react';
+import { StyleSheet, Text, View, Dimensions } from 'react-native';
+import { Calendar } from 'react-native-calendars';
+const { height, width } = Dimensions.get("window");
+
+export default function Calendar() {
+  return (
+    <View style={styles.container}>
+      <Calendar
+        theme={{
+          backgroundColor: '#ffffff',
+          calendarBackground: '#ffffff',
+          textSectionTitleColor: '#b6c1cd',
+          selectedDayBackgroundColor: '#00adf5',
+          selectedDayTextColor: '#ffffff',
+          todayTextColor: '#00adf5',
+          dayTextColor: '#2d4150',
+          textDisabledColor: '#d9e1e8',
+          dotColor: '#00adf5',
+          selectedDotColor: '#ffffff',
+          arrowColor: 'orange',
+          disabledArrowColor: '#d9e1e8',
+          monthTextColor: 'black',
+          indicatorColor: 'blue',
+          textDayFontWeight: '300',
+          textMonthFontWeight: 'bold',
+          textDayHeaderFontWeight: '300',
+          textDayFontSize: 15,
+          textMonthFontSize: 20,
+          textDayHeaderFontSize: 10
+        }}
+        style={{
+          height: 600,
+          borderWidth: 1,
+          borderColor: 'gray'
+        }}
+      />
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    // width: width,
+    // height: height,
+    backgroundColor: '#ffff',
+    // alignItems: 'center',
+    justifyContent: 'center',
+  },
+});
