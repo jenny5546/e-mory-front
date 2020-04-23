b4feafaff023a1e4668ad74193094e8828d2589b
snowman39 <kingzzang5@naver.com>
2020-04-24 04:00:38 +0900

    feed detail first draftw finished

--- components/FeedDetail.js
+++ components/FeedDetail.js
@@ -1,19 +1,31 @@
 import React from 'react';
-import { StyleSheet, Text, TextInput, Button, View, StatusBar } from 'react-native';
+import { StyleSheet, Text, TextInput, Button, View, StatusBar, Image } from 'react-native';
+import EditIcon from './../images/EditIcon.png';
+import DeleteIcon from './../images/DeleteIcon.png';
+import Emoji from './../images/EmojiTemp.png';
 
 export default function FeedNew() {
     return (
         <View style={styles.container}>
             <StatusBar barStyle="light-content"/>
             <View style={styles.popup}>
-                <Text style={styles.date}>2020년 4월 9일</Text>
-                <TextInput
-                style={styles.input}
+                <View style={styles.feedHeader}>
+                    <Text style={styles.date}>2020년 4월 9일</Text>
+                    <View style={styles.icons}>
+                        <Image style={styles.icon} source={EditIcon} />
+                        <Image style={styles.icon} source={DeleteIcon} />
+                    </View>
+                </View>
+                
+                <Text style={styles.text}>오늘은 기분이 좋다!</Text>
+                <Image style={styles.emoji} source={Emoji} />
+                {/* <TextInput
+                
                 placeholder="오늘의 감정일기 100자"
                 placeholderTextColor={"#999"}
                 returnKeyType={"done"}
                 autoCorrect={false}
-                />
+                /> */}
                 <Button
                 // onPress={onPressLearnMore}
                 title="오늘의 감정은?"
@@ -44,9 +56,27 @@ const styles = StyleSheet.create({
         color: "#999999",
         fontSize: 15,
     },
-    input: {
+    feedHeader: {
+        flex: 1,
+        flexDirection: "row",
+        // justifyContent: "space-around",
+    },
+    text: {
         paddingTop: 20,
         fontSize: 10,
+        color: "#999999",
         // flexWrap: "wrap",
+    },
+    icon: {
+        height: 20,
+        width: 20,
+    },
+    icons: {
+        flex: 1,
+        flexDirection: "row",
+    },
+    emoji: {
+        height: 50,
+        width: 50,
     },
 });
